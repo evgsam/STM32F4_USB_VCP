4a57a493da2d84059dbc27497ae487710fdbbd20
user <user@samarinhome>
2020-09-13 17:41:09 +0300

    хрипит пердит но поёт

--- user/source/handlers.c
+++ user/source/handlers.c
@@ -23,27 +23,31 @@ void TIM2_IRQHandler(void) {
 	static bool flag1;
 	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-		ADC_RegularChannelConfig(ADC2, ADC_Channel_14, 1,  ADC_SampleTime_3Cycles);			//PC4
+		ADC_RegularChannelConfig(ADC2, ADC_Channel_14, 1,
+				ADC_SampleTime_3Cycles);			//PC4
 		ADC_SoftwareStartConv(ADC2);
 
-		if (i <ui32RecBuffSize) {
-			ui8ADCConvertedArray_0[i] =ADC_GetConversionValue(ADC2);
-			flag1=true;
+		if (i < ui32RecBuffSize) {
+			ui8ADCConvertedArray_0[i] = ADC_GetConversionValue(ADC2);
+			flag1 = true;
 			i++;
 		} else {
 			if (flag1) {
-				sendHello(&ui8ADCConvertedArray_0[0],sizeof(ui8ADCConvertedArray_0));
+				sendHello(&ui8ADCConvertedArray_0[0],
+						sizeof(ui8ADCConvertedArray_0));
 				STM_EVAL_LEDToggle(LED3);
-				flag1=false;
+				flag1 = false;
+			}
+			if (i < (2 * ui32RecBuffSize)) { //
+				ui8ADCConvertedArray_1[i - ui32RecBuffSize] =
+						ADC_GetConversionValue(ADC2); // (uint8_t)(value);//, (uint8_t)value;// {ADC_GetConversionValue(ADC2)&0xff,;
+				i++;
 			}
 			if (i == (2 * ui32RecBuffSize)) {
-				sendHello(&ui8ADCConvertedArray_1[0],sizeof(ui8ADCConvertedArray_1));
+				sendHello(&ui8ADCConvertedArray_1[0],
+						sizeof(ui8ADCConvertedArray_1));
 				STM_EVAL_LEDToggle(LED6);
 				i = 0;
-			}
-			if (i < (2 * (sizeof(ui8ADCConvertedArray_0)))) { //
-				ui8ADCConvertedArray_1[i - ui32RecBuffSize] =ADC_GetConversionValue(ADC2);// (uint8_t)(value);//, (uint8_t)value;// {ADC_GetConversionValue(ADC2)&0xff,;
-				i++;
 			}
 		}
 
