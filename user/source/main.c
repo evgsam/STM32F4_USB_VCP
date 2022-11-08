bff58a8b5915dcbbaeb7c76dc41b9e08b17903ef
evgsam <evgsamarin@gmail.com>
2022-11-08 19:14:46 +0300

    тесты

--- user/source/main.c
+++ user/source/main.c
@@ -184,18 +184,18 @@ int main(void) {
 	SystemInit();
 	vPererheryInit();
 
-
 	//xTaskCreate(vTaskADCDataSend, "vTaskADCDataSend", 4500, NULL, configMAX_PRIORITIES, NULL);
 	//xTaskCreate(vTaskVCPRxCommandHandler, "vTaskVCPRxCommandHandler", 2000, NULL, configMAX_PRIORITIES - 2, NULL);
 	//xTaskCreate(vTaskVCPRxCommandParser, "vTaskVCPRxCommandParser", 1000, NULL, configMAX_PRIORITIES - 2, NULL);
 
 	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle3", 100, LED3, 0, NULL);
-	//xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle4", 100, LED4, 0, NULL);
-	//xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle5", 100, LED5, 0, NULL);
-	//xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle6", 100, LED6, 0, NULL);
-	//xTaskCreate(vTaskLED7Toggle, "vTaskLEDToggle7", 100, NULL, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle4", 100, LED4, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle5", 100, LED5, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle6", 100, LED6, 0, NULL);
+//	xTaskCreate(vTaskLED7Toggle, "vTaskLEDToggle7", 100, NULL, 0, NULL);
 
 	vTaskStartScheduler();
+
 	while (1) {
 	}
 }
