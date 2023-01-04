3f8c56e896d351137eff3782c174540778b68ff6
evgsam <evgsamarin@gmail.com>
2023-01-04 21:03:41 +0300

    refactoring

--- user/source/main.c
+++ user/source/main.c
@@ -11,13 +11,12 @@ int main(void) {
 	xTaskCreate(vTaskVCPRxCommandHandler, "vTaskVCPRxCommandHandler", 2000, NULL, configMAX_PRIORITIES - 2, NULL);
 	xTaskCreate(vTaskVCPRxCommandParser, "vTaskVCPRxCommandParser", 1000, NULL, configMAX_PRIORITIES - 2, NULL);
 
-	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle3", 100, LED3, 0, NULL);
-	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle4", 100, LED4, 0, NULL);
-	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle5", 100, LED5, 0, NULL);
-	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle6", 100, LED6, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle3", 100, (void*)LED3, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle4", 100, (void*)LED4, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle5", 100, (void*)LED5, 0, NULL);
+	xTaskCreate(vTaskLEDToggle, "vTaskLEDToggle6", 100, (void*)LED6, 0, NULL);
 
 	vTaskStartScheduler();
-
 	while (1) {
 	}
 }
